375a5474d6494d8a3f17aa1400ff6feebe64e0e5
聪李 <congli@conglideMac-Pro.local>
2016-05-20 11:08:35 +0800

    normal pair

--- MainScreen.elm
+++ MainScreen.elm
@@ -6,21 +6,37 @@ import Html.App as App
 
 
 -- MODEL
-type Model =
-	{ primaryModel : PrimaryScreen.Model
-	, secondaryModel : SecondaryScreen.Model
-	}
+type alias Model =
+  { primaryModel : PrimaryScreen.Model
+  , secondaryModel : SecondaryScreen.Model
+  }
 
-init p s = (Model p s, Cmd.none)
-
+init p s = Model (PrimaryScreen.init p) (SecondaryScreen.init s)
 
 -- UPDATE
-port setStorage : Model -> Cmd msg
-
 type Msg
-	= PrimaryChange String
-	| SecondaryChange String
-	| DatabaseChange
+  = PrimaryChange PrimaryScreen.Msg
+  | SecondaryChange SecondaryScreen.Msg
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
+  case msg of
+    PrimaryChange msg -> { model | primaryModel = PrimaryScreen.update msg model.primaryModel }
+    SecondaryChange msg -> { model | secondaryModel = SecondaryScreen.update msg model.secondaryModel }
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div
+    []
+    [ App.map PrimaryChange (PrimaryScreen.view model.primaryModel)
+    , App.map SecondaryChange (SecondaryScreen.view model.secondaryModel)
+    ]
+
+
+main = App.beginnerProgram
+  { model = init "" ""
+  , update = update
+  , view = view
+  }
--- SecondaryScreen.elm
+++ SecondaryScreen.elm
@@ -1,29 +1,27 @@
-port module SecondaryScreen exposing (Model, Msg, init, update, view)
+module SecondaryScreen exposing (Model, Msg, init, update, view)
 import Html exposing (..)
 import Html.App as App
+import Html.Events exposing (onInput)
 
 
 -- MODEL
 type alias Model =
   { title : String
-  , label : String
   }
 
-init : String -> String -> ( Model, Cmd Msg )
-init title label =
-  (Model title label, Cmd.none)
+init : String -> Model
+init title =
+  Model title
 
 
 -- UPDATE
 type Msg
   = Input String
 
-port publish : String -> Cmd msg
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Input str -> ( { model | title = str }, publish str )
+    Input str -> { model | title = str }
 
 
 -- VIEW
@@ -31,6 +29,13 @@ view : Model -> Html Msg
 view model =
   div
     []
-    [ input [] []
-    , text model.label
+    [ input [onInput Input] []
+    , text "Secondary: "
+    , text model.title
     ]
+
+main = App.beginnerProgram
+  { model = init ""
+  , update = update
+  , view = view
+  }
